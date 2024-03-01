8cadb0d7d2b2f752abf17ef6196b03449cde4388
clinton-moss <58459372+clinton-moss@users.noreply.github.com>
2024-03-01 16:37:19 +0200

    Recuring Payments

--- ClientApp/src/components/Payments/Recurring/RecuringPayments.jsx
+++ ClientApp/src/components/Payments/Recurring/RecuringPayments.jsx
@@ -6,11 +6,23 @@ export default function RecuringPayments() {
     return (
         <div>
             <h1>Recurring Payments</h1>
-            <div>
-                <input className='form-control' placeholder='Name of payment' />
-                <input className='form-control' placeholder='Amount' />
-                <input className='form-control' placeholder='Occours in the X day of the month' />
-                <button className='btn'>Create</button>
+            <div className='d-flex'>
+                <div className=' w-50'>
+                    <table className='table'>
+                        <thead>
+                            <td>Name</td>
+                            <td>Amount</td>
+                            <td>Occours on</td>
+                        </thead>
+                    </table>
+                </div>
+                <div className='rounded shadow p-2 w-50 text-center'>
+                    <h2>Create New Payment</h2>
+                    <input className='form-control' placeholder='Name of payment' />
+                    <input className='form-control' placeholder='Amount' />
+                    <input className='form-control' placeholder='Occours on the X day of the month' />
+                    <button className='btn'>Create</button>
+                </div>
             </div>
         </div>
     )
